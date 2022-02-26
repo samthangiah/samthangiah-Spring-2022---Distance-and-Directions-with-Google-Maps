b75463b64881bd59ae891684f6153cd0af90ebf5
Geb1002 <geb1002@sru.edu>
2022-02-26 01:55:02 -0500

    Added comments for my code

--- DistanceAndDirections/src/main/java/edu/sru/bayne/DistanceAndDirections/repository/SearchRepository.java
+++ DistanceAndDirections/src/main/java/edu/sru/bayne/DistanceAndDirections/repository/SearchRepository.java
@@ -1,5 +1,11 @@
 package edu.sru.bayne.DistanceAndDirections.repository;
 
+/**
+ * Repository implementation extending from CRUD repository.
+ * It is used to store Search objects (Search.java) and their data.
+ * @author Gregory Bayne
+ * 
+ */
 import org.springframework.data.repository.CrudRepository;
 import edu.sru.bayne.DistanceAndDirections.domain.Search;
 

d250125c08161d8031b1bc0170d6cd384fb0196d
Michael Booser <mbooser@outlook.com>
2022-04-27 21:36:57 -0400

    added checkGeos() it looks for missing codes and fills in from address

--- DistanceAndDirections/src/main/java/edu/sru/bayne/DistanceAndDirections/domain/StudentPickupInformation.java
+++ DistanceAndDirections/src/main/java/edu/sru/bayne/DistanceAndDirections/domain/StudentPickupInformation.java
@@ -1,11 +1,15 @@
 package edu.sru.bayne.DistanceAndDirections.domain;
 
+import java.io.IOException;
+
 import com.opencsv.bean.CsvBindByName;
+
+import edu.sru.bayne.DistanceAndDirections.GeoGrabber;
 
 /**
  * Class to organize each set of long/lat coordinates into clusters.
  * (NOT YET IMPLEMENTED) **may be unnecessary, will likley be included in search class**
- * @author Gregory
+ * @author Gregory and Michael
  *
  */
 public class StudentPickupInformation {
@@ -13,22 +17,61 @@ public class StudentPickupInformation {
 	@CsvBindByName
 	private long id;
 	
+    	public long getId() {
+    		return id;
+    	}
+    	public void setId(long id) {
+    		this.id = id;
+    	}
+    	
     @CsvBindByName
     private int count;
-    
+    	public int getCount() {
+    		return count;
+    	}
+    	public void setCount(int count) {
+    		this.count = count;
+    	}
+    	
     @CsvBindByName
     private String pickupLoc;
-    
+    	public String getPickupLoc() {
+    		return pickupLoc;
+    	}
+    	public void setPickupLoc(String pickupLoc) {
+    		this.pickupLoc = pickupLoc;
+    	}
     @CsvBindByName
     private String roadName;
-    
+    	public String getRoadName() {
+    		return roadName;
+    	}
+    	public void setRoadName(String roadName) {
+    		this.roadName = roadName;
+    	}
+    	
     @CsvBindByName
-    private float longitude;
-    
+    private String longitude;
+    	public String getLongitude() {
+    		return longitude;
+    	}
+    	public void setLongitude(String longitude) {
+    		this.longitude = longitude;
+    	}
+    	
     @CsvBindByName
-    private float latitude;
-
-    public StudentPickupInformation(long id, int count, String pickupLoc, String roadName, float lng, float lat) {
+    private String latitude;
+    	public String getLatitude() {
+    		return latitude;
+    	}
+    	public void setLatitude(String latitude) {
+    		this.latitude = latitude;
+    	}
+	
+    /**
+     * Constructor for StudenPickupInformation.java
+     */
+    public StudentPickupInformation(long id, int count, String pickupLoc, String roadName, String lng, String lat) {
 	        this.id = id;
 	        this.count = count;
 	        this.pickupLoc = pickupLoc;
@@ -36,9 +79,30 @@ public class StudentPickupInformation {
 	        this.longitude = lng;
 	        this.latitude = lat;
 	    }
+    /**
+     * Confirms the existence of GeoCodes for the Point
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public void checkGeos() throws IOException, InterruptedException 
+    {
+    	if(this.getLatitude() == "0" || this.getLongitude() == "0" || this.getLatitude() == null || this.getLongitude() == null)
+    	{
+    		GeoGrabber codeCheck = new GeoGrabber();
+    		codeCheck.fetchCoordinatesFromAddress(codeCheck.buildFromAddress(this.pickupLoc));
+    		this.setLatitude(codeCheck.getLat());
+    		this.setLongitude(codeCheck.getLng());
+    		
+    	}
+    }
+    
+    public void completeAddress(String newZip)
+    {
+    	
+    }
 	    
-	    // getters and setters removed for the sake of brevity
-	 }
+
+}
 	
 	
 
